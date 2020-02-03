24bd7bd08274681ae84d9e64b503361e9cdab417
Pedro Amorim <pepas52@gmail.com>
2020-02-03 17:34:49 +0000

    Add to favourites

--- MusicLyricsApp/src/app/favourites.service.ts
+++ MusicLyricsApp/src/app/favourites.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { NgForOf } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
@@ -10,14 +11,23 @@ export class FavouritesService {
   constructor() { }
 
   addToFavourites(obj) {
-    console.log(obj)
-    this.favouriteList.push(obj)
-    console.log("-----------");
+    for (let i = 0; i <= this.favouriteList.length; i++) {
+      if (i>0 && Object.values(this.favouriteList[i-1])[2] === Object.values(obj)[2]) {
+        document.getElementById('favouriteBtn').innerHTML = 'Add'
+        this.favouriteList.splice(i-1, 1)
+        break
+      } else {
+        document.getElementById('favouriteBtn').innerHTML = 'Remove'
+        if (i===this.favouriteList.length) {
+          this.favouriteList.push(obj)
+          break
+        }
+      }
+    }
     console.log(this.favouriteList);
   }
 
-  getFavouriteList(){
+  getFavouriteList() {
     return this.favouriteList
   }
 }
- 
